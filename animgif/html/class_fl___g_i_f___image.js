36e485c13677e9ab18b6cafc20ce83ada6e7954d
wcout <wcout@users.noreply.github.com>
2016-12-26 08:03:57 +0100

    ~update animgif doxygen doc

--- animgif/html/class_fl___g_i_f___image.js
+++ animgif/html/class_fl___g_i_f___image.js
@@ -1,10 +1,12 @@
 var class_fl___g_i_f___image =
 [
-    [ "Fl_GIF_Image", "class_fl___g_i_f___image.html#a777518acd38e50dec352d025eb70026e", null ],
+    [ "Fl_GIF_Image", "class_fl___g_i_f___image.html#aeb4fe7205d5a0a0a588ddeceb0132e2d", null ],
     [ "Fl_GIF_Image", "class_fl___g_i_f___image.html#a866d5830543ce782dd5d743295f6eb6b", null ],
     [ "~Fl_GIF_Image", "class_fl___g_i_f___image.html#a59351b13d2aeb7689094f3bd6b594538", null ],
+    [ "Fl_GIF_Image", "class_fl___g_i_f___image.html#acc75e0de68220ae0b1f6518bd4f49824", null ],
     [ "close_gif_file", "class_fl___g_i_f___image.html#aa26f80c51ffb3582006fc0097414c46d", null ],
-    [ "load", "class_fl___g_i_f___image.html#abacaf4c1ca4a6ec34d9de7174016ec37", null ],
+    [ "load", "class_fl___g_i_f___image.html#af7c0983e4af171fa4bd4e1cac1c6f1f5", null ],
     [ "read_next_image", "class_fl___g_i_f___image.html#a1f449cbefa2e2f8fbe0b0947d7d635c1", null ],
+    [ "_anim", "class_fl___g_i_f___image.html#ac0480b334f15c0f2a02733872930dddd", null ],
     [ "gif_handle", "class_fl___g_i_f___image.html#a88e74cdecd7840ea4660e6d78043ce21", null ]
 ];
--- animgif/html/search/functions_2.js
+++ animgif/html/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['fl_5fanim_5fgif_5fimage',['Fl_Anim_GIF_Image',['../class_fl___anim___g_i_f___image.html#a65e30d4a3be53fad6ca12b068e06b22e',1,'Fl_Anim_GIF_Image']]],
-  ['fl_5fgif_5fimage',['Fl_GIF_Image',['../class_fl___g_i_f___image.html#a777518acd38e50dec352d025eb70026e',1,'Fl_GIF_Image']]],
+  ['fl_5fgif_5fimage',['Fl_GIF_Image',['../class_fl___g_i_f___image.html#aeb4fe7205d5a0a0a588ddeceb0132e2d',1,'Fl_GIF_Image::Fl_GIF_Image(const char *filename)'],['../class_fl___g_i_f___image.html#acc75e0de68220ae0b1f6518bd4f49824',1,'Fl_GIF_Image::Fl_GIF_Image(const char *filename, bool anim_)']]],
   ['frame',['frame',['../class_fl___anim___g_i_f___image.html#ade796e54de41ec81b820ed85e828a700',1,'Fl_Anim_GIF_Image::frame(int frame_)'],['../class_fl___anim___g_i_f___image.html#add190bc1241181d4cd22d3855609d808',1,'Fl_Anim_GIF_Image::frame() const ']]],
   ['frame_5funcache',['frame_uncache',['../class_fl___anim___g_i_f___image.html#a6e208aa4442f0734b9c3b1b03284e40e',1,'Fl_Anim_GIF_Image::frame_uncache(bool uncache_)'],['../class_fl___anim___g_i_f___image.html#ae20f7c8ba84134bfb6b5ce694a53da19',1,'Fl_Anim_GIF_Image::frame_uncache() const ']]],
   ['frames',['frames',['../class_fl___anim___g_i_f___image.html#a976642faa0a760dc6ef54b1b8e7fc190',1,'Fl_Anim_GIF_Image']]]
